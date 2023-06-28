87d84465d915fe829c1978295c043e6def7fe7e2
surafel27 <surafelfekadu399@gmail.com>
2023-06-28 08:41:57 +0000

    reject the promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,11 +1,10 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    if (!filename) {
-      resolve(undefined);
+    if (filename) {
+      const err = new Error(`${filename} cannot be processed`);
+      reject(err);
     } else {
-      const err = new Error('cannot be processed');
-      const allError = `${filename} ${err}`;
-      reject(allError);
+      resolve();
     }
   });
 }
