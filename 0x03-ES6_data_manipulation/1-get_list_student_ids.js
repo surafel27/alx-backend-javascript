840045851315424d4365b90583d1fa0379029485
surafel27 <surafelfekadu399@gmail.com>
2023-07-03 17:43:37 +0000

    More mapping ove an array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(getListStudents) {
   if (!(getListStudents instanceof Array)) return [];
-  const studentList = getListStudents.map((value, index) => index);
+  const studentList = getListStudents.map((student) => student.id);
   return studentList;
 }
