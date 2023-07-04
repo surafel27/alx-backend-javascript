102b3071d19c78dc6e03668259392a458922d477
surafel27 <surafelfekadu399@gmail.com>
2023-07-04 08:47:27 +0000

    Clean set using startWith, substr, slice

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  if ( !set || !startString || !(set instanceof Set) || typeof startString !== 'string') return result;
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') return result;
   for (const val of set.values()) {
     if (typeof val === 'string' && val.startsWith(startString)) {
       const word = `${val.substring(startString.length)}-`;
