c7ed3daefa4424048e69bdac5263e3b7ab560019
surafel27 <surafelfekadu399@gmail.com>
2023-07-04 06:49:24 +0000

    combine filter adn map method

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,10 @@ export default function updateStudentGradeByCity(getListStudents, city, newGrade
   const studentByLoc = getListStudents.filter((student) => (student.location === city));
   const updatedStudents = studentByLoc.map((student) => {
     const matchGrade = newGrades.find((grade) => grade.studentId === student.id);
-      if (matchGrade) {
-	  student.grade = matchGrade.grade;
-	} else {
-        student.grade = "N/A";
-      }
-    return student;
+    if (matchGrade) {
+      return { ...student, grade: matchGrade.grade };
+    }
+    return { ...student, grade: 'N/A' };
   });
   return updatedStudents;
 }
