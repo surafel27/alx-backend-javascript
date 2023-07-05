6f71a118c689a4ef289ae54f41f73e96008ac0b8
surafel27 <surafelfekadu399@gmail.com>
2023-07-05 05:39:26 +0000

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,9 @@
 export default function groceriesList() {
-  const groceries = [
-    ['Apple', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ];
-  return new Map(groceries);
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
 }
