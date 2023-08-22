75b4bbbb30074784566fa366370e817beae5c41f
surafel27 <surafelfekadu399@gmail.com>
2023-08-22 08:40:40 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+process.stdout.write("Welcome to Holberton School, what is your name?" + '\n')
+process.stdin.on('readable', function(){
+	const name =  process.stdin.read();
+	process.stdout.write("Your name is:" + name);
+	process.stdout.write("This important software is now closing" + '\n')
+})
