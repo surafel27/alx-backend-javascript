138818be03aeeca3daea5b5e089b2ca6d712cf52
surafel27 <surafelfekadu399@gmail.com>
2023-08-23 06:04:41 +0000

    process.stdin.on

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name) {
-    process.stdout.write(`Your name is: ${name}`)
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
