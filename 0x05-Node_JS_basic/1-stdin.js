e7c6e33589368163faef599d3080f13383ff9b8b
surafel27 <surafelfekadu399@gmail.com>
2023-08-23 06:01:33 +0000

    process.stdin.on

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,13 @@
-/**
- * use process.stdin.on
- * @param {*} name arg
- * @returns string
- */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log("Welcome to Holberton School, what is your name?");
-
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const name =  process.stdin.read();
 
   if (name) {
-    process.stdout.write("Your name is:" + name)
+    process.stdout.write(`Your name is: ${name}`)
   }
 });
 
-process.stdin.on('end', function() {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
