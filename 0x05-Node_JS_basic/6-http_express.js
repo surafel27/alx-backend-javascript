ab0c328d9b01c62b9fd2d63d873697d1fdaa8c07
surafel27 <surafelfekadu399@gmail.com>
2023-08-25 06:53:09 +0000

    HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,16 +1,14 @@
 const express = require('express');
 
 const PORT = 1245;
-const HOST = 'localhost';
-
 const app = express();
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
   req.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://${HOST}:${PORT}\n`);
+  console.log(`Server listening on http://localhost:${PORT}\n`);
 });
 
 module.exports = app;
